ecfbad9ebed871f66c8c043a048b078e6e66d66d
Abhay Pratap Singh <121604260+abhaypratap1968@users.noreply.github.com>
2024-01-28 15:21:15 +0530

    commit

--- Codechef.java
+++ Codechef.java
@@ -7,21 +7,24 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		for(int i=0;i<t;i++){
-		    int p = sc.nextInt();
-		    int l = sc.nextInt();
-		    float s = (l/p)*100;
-		    
-		    if(s>=75){
-		        System.out.println("YES");
-		    }
-		    else{
-		        System.out.println("NO");
-		    }
-		}
-
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++){
+            int x = sc.nextInt();
+            if(x%4==0){
+                System.out.println("North");
+            }
+            else if(x%2==0 && x%4!=0){
+                System.out.println("South");
+            }
+            else if(x%3==0){
+                System.out.println("West");
+                
+            }
+            else{
+                System.out.println("East");
+            }
+        }
+		sc.close();
 	}
 }
-
